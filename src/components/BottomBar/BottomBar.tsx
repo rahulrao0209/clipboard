a588dbc2b48e81a7d5df4cfd12f53e23724a469a
Rahul <rahulrao0209@gmail.com>
2022-10-23 17:30:25 +0530

    Adding initial version of the bottom bar
    Added a bottom bar with an add button. Added CSS styling for button along with react-icons pencil icon. This button should be used to show a modal/popup that would allow a user to write/paste a note in the clipboard

--- src/components/BottomBar/BottomBar.tsx
+++ src/components/BottomBar/BottomBar.tsx
@@ -1,7 +1,14 @@
+import { FaPencilAlt } from 'react-icons/fa';
 import './BottomBar.scss';
 
 const BottomBar = function () {
-  return <div></div>;
+  return (
+    <div className="bottom-bar">
+      <div className="add-btn">
+        <FaPencilAlt className="pencil-icon" />
+      </div>
+    </div>
+  );
 };
 
 export default BottomBar;
