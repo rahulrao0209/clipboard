de726e40ed01f20a6efb5f48e2f0b7bdb3a19b3d
Rahul Rao <63695122+rahulrao0209@users.noreply.github.com>
2022-12-02 22:52:37 +0530

    adding options buttons and styling to the settings modal
    The commit consists of the changes adding an initial version of styling and options buttons to the settings modal. The settings modal now consists of the delete all clips and confirm before delete option buttons along with the appropriate css styles applied

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,16 +1,38 @@
 import { useContext } from 'react';
 import { SettingsContext } from '../../context';
+import { AiFillTool, MdDelete } from '../../icons/index';
 import './Settings.scss';
 
 const Settings = function () {
   const { settingsModalOpen } = useContext(SettingsContext);
 
-  console.log(settingsModalOpen);
   return (
     <div
       className={`settings ${
         settingsModalOpen ? 'settings--open' : 'settings--close'
-      }`}></div>
+      }`}>
+      <div className="settings__title">
+        <span className="settings-tool">
+          <AiFillTool className="settings-tool-icon" />
+        </span>
+        <span>Settings</span>
+      </div>
+      <div className="settings__options">
+        <div className="settings__option">
+          <button className="settings--delete-all-btn">
+            <span>Delete All Clips</span>
+            <span className="delete-icon-container">
+              <MdDelete className="delete-icon--settings" />
+            </span>
+          </button>
+        </div>
+        <div className="settings__option">
+          <button className="settings--confirm-before-delete">
+            Confirm before delete?
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,7 +1,16 @@
 /* Export all the icons required throughout the project */
+import { AiFillTool } from 'react-icons/ai';
 import { BiSend } from 'react-icons/bi';
 import { FaPencilAlt } from 'react-icons/fa';
 import { MdContentCopy, MdDelete, MdSettings } from 'react-icons/md';
 import { TfiClose } from 'react-icons/tfi';
 
-export { FaPencilAlt, MdContentCopy, MdDelete, MdSettings, BiSend, TfiClose };
+export {
+  FaPencilAlt,
+  MdContentCopy,
+  MdDelete,
+  MdSettings,
+  BiSend,
+  TfiClose,
+  AiFillTool,
+};
